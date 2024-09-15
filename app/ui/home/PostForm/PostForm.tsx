fc551d2ff34b44b114b65d4d6229d71a84e02e38
Emil307 <emilnovikov392@gmail.com>
2024-09-15 23:51:04 +0600

    [Feat] resize input

--- app/ui/home/PostForm/PostForm.tsx
+++ app/ui/home/PostForm/PostForm.tsx
@@ -1,17 +1,19 @@
-import { Button } from "@chakra-ui/react";
+"use client";
+
 import React from "react";
+import { Button } from "@chakra-ui/react";
+import TextareaAutosize from "react-textarea-autosize";
 
 export const PostForm = () => {
   return (
     <div className="flex flex-col items-end w-full h-fit p-4 border-b-sm border-borderColor">
-      <div className="w-full flex gap-2 items-center">
-        <div className="flex items-center justify-center w-11 h-11">
+      <div className="w-full flex gap-2">
+        <div className="flex items-center justify-center w-11 h-11 mt-1">
           <div className="w-10 h-10 rounded-full bg-orange-800"></div>
         </div>
-        <input
-          className="w-full text-xl py-3 caret-textGray"
+        <TextareaAutosize
           placeholder="What is happening?!"
-          type="text"
+          className="w-full h-full h-24 resize-none text-xl py-3 caret-textGray overflow-hidden"
         />
       </div>
       <Button
