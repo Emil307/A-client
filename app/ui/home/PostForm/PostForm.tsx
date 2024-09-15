6acefeefb72e9abae2449fcc437b6307a1769da0
Emil307 <emilnovikov392@gmail.com>
2024-09-15 23:10:40 +0600

    [Chore] layout post button

--- app/ui/home/PostForm/PostForm.tsx
+++ app/ui/home/PostForm/PostForm.tsx
@@ -1,18 +1,30 @@
+import { Button } from "@chakra-ui/react";
 import React from "react";
 
 export const PostForm = () => {
   return (
-    <div className="w-full h-fit py-2 px-4 border-b-sm border-borderColor">
-      <div className="flex gap-2 items-center">
+    <div className="flex flex-col items-end w-full h-fit p-4 border-b-sm border-borderColor">
+      <div className="w-full flex gap-2 items-center">
         <div className="flex items-center justify-center w-11 h-11">
           <div className="w-10 h-10 rounded-full bg-orange-800"></div>
         </div>
         <input
-          className="text-xl py-3 caret-textGray"
+          className="w-full text-xl py-3 caret-textGray"
           placeholder="What is happening?!"
           type="text"
         />
       </div>
+      <Button
+        paddingX={"2rem"}
+        size={"lg"}
+        fontSize={"1rem"}
+        fontWeight={"bold"}
+        rounded={"full"}
+        color={"white"}
+        colorScheme="blue"
+      >
+        Post
+      </Button>
     </div>
   );
 };
