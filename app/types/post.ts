89fc66706356cd6c5644fc9462ce621306ad11fc
Emil307 <emilnovikov392@gmail.com>
2024-09-19 10:00:15 +0600

    [Feat] replyes

--- app/types/post.ts
+++ app/types/post.ts
@@ -5,4 +5,5 @@ export interface IPost {
   body: string;
   createdAt: Date;
   owner: IUser;
+  predecessor: IPost;
 }
--- app/ui/home/PostCard/Post.tsx
+++ app/ui/home/PostCard/Post.tsx
@@ -1,35 +1,59 @@
 import React from "react";
+import Image from "next/image";
 import { IPost } from "@/app/types/post";
+import { ReplyPopup } from "@/app/ui/home/ReplyPopup";
+import { useDisclosure } from "@chakra-ui/react";
 
 interface IPostCard {
   post: IPost;
 }
 
 export const PostCard: React.FC<IPostCard> = ({ post }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div className="w-full flex flex-col gap-2 px-4 py-3 border-b-sm border-borderColor">
-      <div>
-        <div className="flex gap-2">
-          <div className="flex items-center justify-center w-11 h-11">
-            <div className="w-10 h-10 rounded-full bg-orange-800"></div>
-          </div>
-          <div className="flex flex-col">
-            <div className="flex gap-2">
-              <h1 className="font-bold text-textWhite">{post.owner.name}</h1>
-              <span className="text-textGray">
-                {post.owner.tag} ·{" "}
-                {new Date(post.createdAt).toLocaleDateString("en-us", {
-                  weekday: "long",
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                })}
-              </span>
+    <>
+      <div className="w-full flex flex-col gap-2 px-4 py-3 border-t-sm border-borderColor">
+        <div>
+          <div className="flex gap-2">
+            <div className="flex items-center justify-center w-11 h-11">
+              <div className="w-10 h-10 rounded-full bg-orange-800"></div>
             </div>
-            <p className="w-full break-all">{post.body}</p>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-2">
+                <h1 className="font-bold text-textWhite">
+                  {post?.owner?.name}
+                </h1>
+                <span className="text-textGray">
+                  {post.owner?.tag} ·{" "}
+                  {new Date(post.createdAt).toLocaleDateString("en-us", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </span>
+              </div>
+              <p className="w-full break-all">{post.body}</p>
+              <button onClick={onOpen}>
+                <Image
+                  src="/assets/icons/reply.svg"
+                  priority
+                  width={24}
+                  height={24}
+                  alt="reply"
+                />
+              </button>
+            </div>
           </div>
         </div>
+        {post.predecessor && (
+          <div className="p-3 rounded-md border-sm border-borderColor">
+            {post.predecessor.body}
+          </div>
+        )}
       </div>
-    </div>
+      <ReplyPopup predecessorId={post.id} isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
